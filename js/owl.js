feee0a695bf1cbb56a525f99ca637e24d6343368
galih asmarandaru <galih.asmarandaru@gmail.com>
2021-02-26 13:16:32 +0700

    add responsive search

--- js/owl.js
+++ js/owl.js
@@ -1,20 +1,11 @@
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
+        center: true,
         loop: true,
+        autoWidth: true,
         margin: 10,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 3
-            }
-        },
         autoplay: true,
         autoplayTimeout: 4000,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
     })
 });
